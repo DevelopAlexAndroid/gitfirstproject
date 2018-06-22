bef3cc9af6f6ca4f3980727962b232ff25e64efd
Alex Savchenko <cavhenko90903@yandex.ru>
2018-06-22 11:26:28 +0300

    new class

---
+++ app/src/main/java/com/example/usersad/myapplication/activity/firstnewClass.java
@@ -0,0 +1,13 @@
+package com.example.usersad.myapplication.activity;
+
+public class firstnewClass {
+    public String getLol() {
+        return lol;
+    }
+
+    public void setLol(String lol) {
+        this.lol = lol;
+    }
+
+    private String lol;
+}
